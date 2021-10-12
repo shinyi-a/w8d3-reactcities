e2e3e713466563d05c271420dcd91e0acaa6473b
Shin Yi <contact.shinyi@gmail.com>
2021-10-12 14:09:34 +0800

    added click

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 import imagesArr from './imageData';
 
 function App() {
-  const [bigImage, setBigImage] = useState(imagesArr[6].img);
+  // USE useState TO CREATE  [bigImage, setBigImage]
+  // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  const [bigImage, setBigImage] = useState(imagesArr[0].img);
+
+	// CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
+	// THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  function Clicked(event) {
+    const clickedimg = event.target.src;
+    setBigImage(clickedimg);
+  }
+
   return (
     <div className="App">
       <h1>Cities Of The World</h1>
@@ -11,7 +21,7 @@ function App() {
         <div id="thumbnails">
         {/* RENDER THE IMAGES ARRAY  */}
         {imagesArr.map((img) => {
-          return <img key={img.city} className="thumb" src={img.img} alt={img.city}></img>
+          return <img key={img.city} className="thumb" src={img.img} alt={img.city} onClicked={Clicked}></img>
         })}
         </div>
         {/* THE SRC IMAGE URL SHOULD BE SET TO THE VALUE THAT IS STORED IN bigImage */}
